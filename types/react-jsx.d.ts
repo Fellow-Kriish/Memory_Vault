d9bfb18e77c5ac06b17f41e006460e5663dc0ac1
Developer <dev@localhost>
2025-12-11 16:32:24 +0530

    Remove temporary TS shim declarations to fix build

--- types/react-jsx.d.ts
+++ types/react-jsx.d.ts
@@ -1,17 +1,1 @@
-declare module 'react' {
-  export * from 'any'
-}
-
-declare module 'react/jsx-runtime' {
-  export function jsx(type: any, props?: any, key?: any): any
-  export function jsxs(type: any, props?: any, key?: any): any
-  export const Fragment: any
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: any
-    }
-  }
-}
+export {}
